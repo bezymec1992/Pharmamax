c4d28c1dd0bc22129a705cf949063cc3f7fdcd10
Vasiliy Lavrov <68432186+bezymec1992@users.noreply.github.com>
2024-08-07 20:37:33 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+const buttons = document.querySelectorAll('.btn-language');
+const accordions = document.querySelectorAll(".accordion");
+
+//language btn active
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+    buttons.forEach(btn => btn.classList.remove('active__language'));
+    button.classList.add('active__language');
+  });
+});
+
+//accordions
+for(item of accordions) {
+    item.addEventListener("click", function () {
+        this.classList.toggle("active");
+        let panel = this.nextElementSibling;
+        console.log(this.nextElementSibling)
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    })
+}
+
+//burger add class for open
+const burger = document.querySelector(".burger");
+burger.addEventListener('click', function() {
+  document.body.classList.toggle("open")
+  burger.classList.toggle("menu-opened");
+})
